31cf44cb9b35bb47012ec28b4a20579b5bc83fb5
Sharif Olorin <sio@tesser.org>
2016-10-26 03:51:29 +0000

    Add missing env-parsing tests

--- test/Test/IO/Ambiata/Cli/Env.hs
+++ test/Test/IO/Ambiata/Cli/Env.hs
@@ -32,16 +32,41 @@ prop_load_failure = testIO $ do
   pure $ either (const (True === True)) (\e -> failWith $ "Should not have loaded environment: " <> pack (show e)) x
 
 prop_load_common_environment :: TestEnv -> Property
-prop_load_common_environment (TestEnv ep' key' _dir' _dl') = testIO $ do
-  _ <- setEnv "AMBIATA_API_ENDPOINT" (unpack ep') True
-  _ <- setEnv "AMBIATA_API_KEY" (unpack key') True
+prop_load_common_environment te = testIO $ do
+  setTestEnv te
   x <- E.parseOr pure mempty common
   pure $ (
       fmap (fmap unAmbEndpoint . explicitApiEndpoint) x
     , fmap (unAmbKey . apiKey) x
     ) === (
-      Right (Just ep')
-    , Right key'
+      Right (Just $ tapiEndpoint te)
+    , Right $ tapiKey te
+    )
+
+prop_load_upload_environment :: TestEnv -> Property
+prop_load_upload_environment te = testIO $ do
+  setTestEnv te
+  x <- E.parseOr pure mempty uploadEnv'
+  pure $ (
+      fmap (\(UploadEnv dir'' _ _) -> pack $ unDir dir'') x
+    , fmap (\(UploadEnv _ rt _) -> renderIntegral $ retentionDays rt) x
+    ) === (
+      Right $ tuploadDir te
+    , Right $ tarchiveRetention te
+    )
+
+prop_load_download_environment :: TestEnv -> Property
+prop_load_download_environment te = testIO $ do
+  setTestEnv te
+  x <- E.parseOr pure mempty downloadEnv'
+  pure $ (
+      fmap (\(DownloadEnv dd _ _ _) -> pack $ unDownloadDir dd) x
+    , fmap (\(DownloadEnv _ org _ _) -> unOrganisation org) x
+    , fmap (\(DownloadEnv _ _ e _) -> unEndpoint e) x
+    ) === (
+      Right $ tdownloadDir te
+    , Right $ torganisationId te
+    , Right $ tendpointId te
     )
 
 prop_api_credential :: AmbiataAPICredential -> Property
@@ -57,19 +82,37 @@ prop_api_credential c@(TSRPCredential kid sk re) =
   c' <- E.parse mempty apiCredential
   pure $ c === c'
 
+setTestEnv :: TestEnv -> IO ()
+setTestEnv (TestEnv ep' key' dir' dl' rt oid eid) =
+  mapM_ (\(k,v) -> setEnv k (unpack v) True) [
+      ("AMBIATA_API_ENDPOINT", ep')
+    , ("AMBIATA_API_KEY", key')
+    , ("UPLOAD_DIR", dir')
+    , ("DOWNLOAD_DIR", dl')
+    , ("ARCHIVE_RETENTION", rt)
+    , ("ORGANISATION_ID", oid)
+    , ("ENDPOINT_ID", eid)
+    ]
+
 data TestEnv =
   TestEnv {
-  tapiEndpoint :: Text,
-  tapiKey      :: Text,
-  tuploadDir   :: Text,
-  tdownloadDir :: Text
-} deriving (Show)
+    tapiEndpoint :: Text
+  , tapiKey :: Text
+  , tuploadDir :: Text
+  , tdownloadDir :: Text
+  , tarchiveRetention :: Text
+  , torganisationId :: Text
+  , tendpointId :: Text
+  } deriving (Show)
 
 instance Arbitrary TestEnv where
   arbitrary = TestEnv <$> elements (fmap ("https://" <>) southpark)
                       <*> elements cooking
                       <*> elements muppets
                       <*> elements viruses
+                      <*> (renderIntegral <$> (choose (0 :: Int, 20)))
+                      <*> elements colours
+                      <*> elements weather
 
 
 return []
