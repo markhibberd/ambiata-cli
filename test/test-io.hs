dffdbc00cf66d0142bc221546126f19eee570ca8
Sharif Olorin <sio@tesser.org>
2016-10-26 01:02:37 +0000

    Actually run env parsing tests

--- test/test-io.hs
+++ test/test-io.hs
@@ -3,6 +3,7 @@ import           Disorder.Core.Main
 
 import qualified Test.IO.Ambiata.Cli.Api.Upload
 import qualified Test.IO.Ambiata.Cli.Downloads
+import qualified Test.IO.Ambiata.Cli.Env
 import qualified Test.IO.Ambiata.Cli.Http
 import qualified Test.IO.Ambiata.Cli.Incoming
 import qualified Test.IO.Ambiata.Cli.Processing
@@ -12,9 +13,10 @@ main :: IO ()
 main =
   disorderMain [
       Test.IO.Ambiata.Cli.Api.Upload.tests
+    , Test.IO.Ambiata.Cli.Downloads.tests
+    , Test.IO.Ambiata.Cli.Env.tests
+    , Test.IO.Ambiata.Cli.Http.tests
     , Test.IO.Ambiata.Cli.Incoming.tests
     , Test.IO.Ambiata.Cli.Processing.tests
-    , Test.IO.Ambiata.Cli.Downloads.tests
-    , Test.IO.Ambiata.Cli.Http.tests
     , Test.IO.Ambiata.Cli.Standalone.tests
     ]
