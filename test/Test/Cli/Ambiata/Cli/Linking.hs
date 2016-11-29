9c4630618963c89cf7bc85bf15560160fd7f6b56
Sharif Olorin <sio@tesser.org>
2016-11-29 21:40:12 +0000

    Add test for number of object dependencies

--- test/Test/Cli/Ambiata/Cli/Linking.hs
+++ test/Test/Cli/Ambiata/Cli/Linking.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Test.Cli.Ambiata.Cli.Linking where
+module Test.Cli.Ambiata.Cli.Linking (
+    tests
+  ) where
 
+import qualified Data.List as L
 import qualified Data.Text as T
 
 import           Disorder.Core.IO
@@ -24,6 +27,16 @@ prop_static_gmp exe = testIO $ do
   (st, out) <- testShell' [ "ldd", exe ]
   pure $ (st, "libgmp" `T.isInfixOf` (T.toLower out)) === (ExitSuccess, False)
 
+-- Ensure we don't accidentally add any object dependencies.
+prop_object_dependencies :: Text -> Property
+prop_object_dependencies exe = testIO $ do
+  (st, out) <- testShell' [ "ldd", exe ]
+  let n = L.length $ T.lines out
+  pure $ (st, n) === (ExitSuccess, 9)
+
 tests :: Text -> IO Bool
 tests exe = fmap (all isSuccess) $
-  mapM (quickCheckWithResult stdArgs { maxSuccess = 1 }) $ fmap ($ exe) [ prop_static_gmp ]
+  mapM (quickCheckWithResult stdArgs { maxSuccess = 1 }) $ fmap ($ exe) [
+      prop_static_gmp
+    , prop_object_dependencies
+    ]
