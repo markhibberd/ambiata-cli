9b1c1c4ec536476051ded3749cdc17a86a0caa31
mth <mark@hibberd.id.au>
2017-01-30 18:19:29 +1100

    Update examples and mismi.

--- doc/examples/raw/run_ambiata_upload.sh
+++ doc/examples/raw/run_ambiata_upload.sh
@@ -10,7 +10,7 @@ export AMBIATA_API_KEY_ID=DWPXY18c57b5cde3dc531dbfa19e781f24605e
 export AMBIATA_API_KEY_SECRET=LWTGZD89cf43bed574d6e6a54bf436b3a4ba8dc658973b85aa5bfc80f05e38e01d28d7
 export UPLOAD_DIR=/home/upload/data/
 
-# Use an HTTP proxy which requires authentication.
-export https_proxy=http://username:password@proxy.localdomain:3128
+# If required, use an HTTP proxy which requires authentication.
+# export https_proxy=http://username:password@proxy.localdomain:3128
 
 exec /opt/ambiata/bin/ambiata-daemon upload
